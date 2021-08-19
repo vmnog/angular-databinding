a9d52cae2765a7ec70c207a25b622e4ee7f12caa
Victor Nogueira <p51901224@brasilprev.com.br>
2021-08-19 17:42:56 -0300

    feat: binding custom data properties through components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
-
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
+  serverElements = [
+    { type: "server", name: "Server 1", content: "Server 1 Content" },
+    { type: "server", name: "Server 2", content: "Server 2 Content" },
+    { type: "server", name: "Server 3", content: "Server 3 Content" },
+  ];
 }
